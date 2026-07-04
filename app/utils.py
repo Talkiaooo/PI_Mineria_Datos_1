8de6a0d28c2c512f4a263c275f48af2fa3f4d696
Talkiaooo <134364163+Talkiaooo@users.noreply.github.com>
2026-07-04 14:27:30 -0300

    Add files via upload

--- app/utils.py
+++ app/utils.py
@@ -10,3 +10,29 @@ def load_data():
     
     # Esto cargará el archivo sin importar desde dónde ejecutes la terminal
     return pd.read_csv(ruta_csv)
+
+import os
+from datetime import datetime
+
+def log_transformacion(paso, descripcion, df, log_path='logs/pipeline_log.csv'):
+    
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+    filas = len(df)
+    nulos = df.isnull().sum().sum()
+    
+   
+    data = {
+        'Timestamp': [datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
+        'Paso': [paso],
+        'Descripción': [descripcion],
+        'Filas': [filas],
+        'Nulos': [nulos]
+    }
+    
+    df_log = pd.DataFrame(data)
+    
+    if not os.path.exists(log_path):
+        df_log.to_csv(log_path, index=False)
+    else:
+        df_log.to_csv(log_path, mode='a', header=False, index=False)
